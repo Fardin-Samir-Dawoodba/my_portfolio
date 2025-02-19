b19b5507f024865de26cac170c0cdba1d0d9e8dc
prathameshkarambelkar <pkarambelkar.2002@gmail.com>
2025-02-19 23:47:12 +0530

    fix build

--- next.config.js
+++ next.config.js
@@ -1,23 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone', // Ensures a fully static export
     images: {
         unoptimized: true, // ✅ Disable Image Optimization for Static Export
         domains: ['open.cruip.com', 'ucarecdn.com', 'www.svgrepo.com', 'images.unsplash.com', 'res.cloudinary.com'],
     },
-
+    output: 'export',
+    distDir: 'build', // or whatever directory you're using
     webpack(config, { isServer }) {
         config.module.rules.push({
             test: /\.svg$/,
             use: ["@svgr/webpack"],
         });
-
         // ✅ Enable WebAssembly support
         config.experiments = { asyncWebAssembly: true, topLevelAwait: true };
-
         // ✅ Disable minification for debugging (Optional)
-
         return config;
     },
 };
