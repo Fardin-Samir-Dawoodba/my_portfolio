b5f45ae49d1fc1a18ecc3316fe6c9e2ad64cb05b
prathameshkarambelkar <pkarambelkar.2002@gmail.com>
2025-02-19 23:21:47 +0530

    updated nextjs version, update next.config.js, fix build errors

--- next.config.js
+++ next.config.js
@@ -3,16 +3,24 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ['open.cruip.com','ucarecdn.com', 'www.svgrepo.com','images.unsplash.com', 'res.cloudinary.com']
+        domains: ['open.cruip.com', 'ucarecdn.com', 'www.svgrepo.com', 'images.unsplash.com', 'res.cloudinary.com'],
     },
-    webpack(config) {
-        config
-            .module
-            .rules
-            .push({test: /\.svg$/, use: ["@svgr/webpack"]});
+    webpack(config, { isServer }) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        // ✅ Enable WebAssembly support
+        config.experiments = { asyncWebAssembly: true, topLevelAwait: true };
+
+        // ✅ Disable minification for debugging (Optional, only if needed)
+        if (!isServer) {
+            config.optimization.minimize = false;
+        }
 
         return config;
-    }
-}
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
