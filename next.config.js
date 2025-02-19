cdb48d91c0bd055fa36e4201528b4440673c535e
prathameshkarambelkar <pkarambelkar.2002@gmail.com>
2025-02-19 23:39:48 +0530

    fix build errors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
-    output: 'export', // Ensures a fully static export
+    output: 'standalone', // Ensures a fully static export
     images: {
         unoptimized: true, // ✅ Disable Image Optimization for Static Export
         domains: ['open.cruip.com', 'ucarecdn.com', 'www.svgrepo.com', 'images.unsplash.com', 'res.cloudinary.com'],
     },
+
     webpack(config, { isServer }) {
         config.module.rules.push({
             test: /\.svg$/,
@@ -17,9 +17,6 @@ const nextConfig = {
         config.experiments = { asyncWebAssembly: true, topLevelAwait: true };
 
         // ✅ Disable minification for debugging (Optional)
-        if (!isServer) {
-            config.optimization.minimize = false;
-        }
 
         return config;
     },
